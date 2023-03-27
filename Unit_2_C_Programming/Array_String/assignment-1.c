1c0c00fa50eb3c1dcf00683d21caeed4b3ca2cb1
Mo-Alsehli <mohamed.m.alsehli@gmail.com>
2023-03-27 23:44:14 +0200

    Starting Array_String assignment

---
+++ Unit_2_C_Programming/Array_String/assignment-1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+
+
+int main() {
+    /* printf("Program-1: \n");
+    printf("Write a C program to find the sum of two matrices of order 2 * 2 using multi-dimensional arrays? \n\n");
+
+    float matrix1[2][2], matrix2[2][2];
+
+    printf("Enter Elements Of First Matrix: \n");
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            printf("Enter a%d%d: ", i + 1, j + 1);
+            scanf("%f", &matrix1[i][j]);
+        }
+    }
+
+    printf("Enter Elements Of Second Matrix: \n");
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            printf("Enter b%d%d: ", i + 1, j + 1);
+            scanf("%f", &matrix2[i][j]);
+        }
+    }
+
+    printf("Sum Of Matrix: \n");
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            printf("%.2f      ", matrix1[i][j] + matrix2[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n \n"); */
+
+    printf("Program-2: \n");
+    printf("");
+
+    
+}
